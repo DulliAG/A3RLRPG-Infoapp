3ee1f556f166a568f0a8f9dbcb139d4033007561
Thorben <38816229+tklein1801@users.noreply.github.com>
2020-08-13 14:57:15 +0200

    :lipstick: Add cog for settings

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 require("react-native").unstable_enableLogBox();
-
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View, ToastAndroid } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import useCachedResources from "./hooks/useCachedResources";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
@@ -35,7 +35,28 @@ export default function App() {
                   fontWeight: "bold",
                   flexGrow: 1,
                   alignSelf: "center",
+                  marginRight: -55 /* required to be in center bcause of the headerRight-element */,
                 },
+                headerRight: () => (
+                  <TouchableWithoutFeedback
+                    onPress={() => {
+                      ToastAndroid.showWithGravityAndOffset(
+                        "Einstellungen öffnen",
+                        ToastAndroid.LONG,
+                        ToastAndroid.BOTTOM,
+                        25,
+                        150
+                      );
+                    }}
+                  >
+                    <Ionicons
+                      style={{ marginRight: 25 }}
+                      name="ios-settings"
+                      size={28}
+                      color="black"
+                    />
+                  </TouchableWithoutFeedback>
+                ),
               }}
             />
           </Stack.Navigator>
