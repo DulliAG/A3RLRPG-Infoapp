c35370318b59c8b00ac9e6c16bd01caea1c2b686
Thorben <38816229+tklein1801@users.noreply.github.com>
2020-09-13 21:12:44 +0200

    :lipstick: Update settings

--- App.js
+++ App.js
@@ -3,54 +3,25 @@ import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-  ToastAndroid,
-  Modal,
-  TouchableHighlight,
-  TextInput,
-  Text,
-  Switch,
-} from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import useCachedResources from "./hooks/useCachedResources";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import LinkingConfiguration from "./navigation/LinkingConfiguration";
-import AsyncStorage from "@react-native-community/async-storage";
-import styled from "styled-components";
-import Colors from "./constants/Colors";
+import { SettingsModal } from "./components/SettingsModal";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const [showSettings, setShowSettings] = React.useState(false);
-  const [key, setKey] = React.useState("Placeholder");
-  const [isDark, setDark] = React.useState(false);
-
-  const saveKey = async (value) => {
-    try {
-      await AsyncStorage.setItem("@apiKey", value);
-    } catch (err) {
-      console.error(err);
-    }
+  let popupRef = React.createRef();
+  const onShowPopup = () => {
+    popupRef.show();
+  };
+  const onClosePopup = () => {
+    popupRef.close();
   };
 
-  const getKey = async () => {
-    try {
-      setKey(await AsyncStorage.getItem("@apiKey"));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    getKey();
-  }, []);
-
-  // TODO Create an external component for the settings
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -80,7 +51,7 @@ export default function App() {
                 headerRight: () => (
                   <TouchableWithoutFeedback
                     onPress={() => {
-                      setShowSettings(!showSettings);
+                      onShowPopup();
                     }}
                   >
                     <Ionicons
@@ -95,107 +66,15 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-        <Modal animationType="slide" transparent={true} visible={showSettings}>
-          <View style={styles.modalView}>
-            <Strong>API-Key</Strong>
-            <TextInput
-              style={styles.input}
-              value={key}
-              onChangeText={(text) => {
-                setKey(text);
-              }}
-            />
-
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                marginTop: 20,
-              }}
-            >
-              <Switch
-                trackColor={{ false: "#767577", true: "#bdbdbd" }}
-                thumbColor={isDark ? Colors.tabIconSelected : "#f4f3f4"}
-                onValueChange={() => {
-                  setDark(!isDark);
-                }}
-                value={isDark}
-              />
-              <Text style={{ fontSize: 16, fontWeight: "bold" }}>Dark-Mode</Text>
-            </View>
-
-            <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 20 }}>
-              <TouchableHighlight
-                style={{ ...styles.modalButton, backgroundColor: "#bdbdbd" }}
-                onPress={() => {
-                  setShowSettings(!showSettings);
-                }}
-              >
-                <Text style={styles.modalButtonText}>Abbrechen</Text>
-              </TouchableHighlight>
-              <TouchableHighlight
-                style={{ ...styles.modalButton, backgroundColor: "#2196F3" }}
-                onPress={() => {
-                  setShowSettings(!showSettings);
-                  saveKey(key);
-                  ToastAndroid.showWithGravityAndOffset(
-                    "API-Key gespeichert",
-                    ToastAndroid.LONG,
-                    ToastAndroid.BOTTOM,
-                    25,
-                    150
-                  );
-                }}
-              >
-                <Text style={styles.modalButtonText}>Speichern</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
+        <SettingsModal ref={(target) => (popupRef = target)} />
       </View>
     );
   }
 }
 
-const Strong = styled.Text`
-  margin-bottom: 5px;
-  font-weight: bold;
-  font-size: 18px;
-`;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-  },
-  input: {
-    height: 40,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-    borderColor: "#2f95dc",
-    borderWidth: 2,
-  },
-  modalView: {
-    backgroundColor: "#fff",
-    padding: 35,
-    elevation: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-  },
-  modalButton: {
-    elevation: 2,
-    padding: 10,
-    width: "48%",
-    borderRadius: 5,
-  },
-  modalButtonText: {
-    textAlign: "center",
-    fontWeight: "bold",
-    color: "#fff",
   },
 });
