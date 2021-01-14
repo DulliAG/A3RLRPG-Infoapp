890e783e067cace2fc95e91ed19608a2c81e3c63
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-01-14 17:33:13 +0100

    Add DulliAG RestAPI for contacts

--- ApiHandler.js
+++ ApiHandler.js
@@ -109,5 +109,77 @@ class ReallifeAPI {
   }
 }
 
-export { ReallifeAPI };
+class DulliAPI {
+  constructor() {
+    this.host = "https://api.dulliag.de";
+  }
+
+  /**
+   * @param {string} playerName ReallifeRPG playername
+   * @param {string} playerId Steam64 id
+   * @param {string} avatarUrl Steam avatar url
+   * @param {string} telNo ReallifeRPG telephone number
+   * @param {string} iban IBAN from players bank account
+   */
+  async createContact(playerName, playerId, avatarUrl, telNo, iban) {
+    const formData = new FormData();
+    formData.append("playerName", playerName);
+    formData.append("playerId", playerId);
+    formData.append("avatarUrl", avatarUrl);
+    formData.append("telNo", telNo);
+    formData.append("iban", iban);
+    const response = await fetch(this.host + "/acon/v1/create.php", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  /**
+   * @param {number} contactId
+   */
+  async deleteContact(contactId) {
+    const formData = new FormData();
+    formData.append("contactId", contactId);
+    const response = await fetch(this.host + "/acon/v1/delete.php", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  async getAllContacts() {
+    const response = await fetch(this.host + "/acon/v1/getAll.php", {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  /**
+   * @param {string} playerId Steam64 id
+   */
+  async getContactByPlayerId(playerId) {
+    const response = await fetch(`${this.host}/acon/v1/getAll.php?playerId=${playerId}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  /**
+   * @param {number} contactId
+   */
+  async getContact(contactId) {
+    const response = await fetch(`${this.host}/acon/v1/getAll.php?contactId=${contactId}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  }
+}
+
+export { ReallifeAPI, DulliAPI };
 // Arnold Hobel Michael Metzger
