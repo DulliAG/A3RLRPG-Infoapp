b95cc60c23f81cc5ac54f339c3a69735c2c30114
Thorben <38816229+tklein1801@users.noreply.github.com>
2022-06-03 16:05:43 +0200

    :sparkles: Display time when prices were calculated

--- screens/market.screen.tsx
+++ screens/market.screen.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import * as React from 'react';
 import { Image, ScrollView, View } from 'react-native';
 import { List, Text, useTheme } from 'react-native-paper';
@@ -71,6 +72,21 @@ export const Market: React.FC = () => {
                 style={{ borderBottomWidth: 1, borderBottomColor: theme.colors.border }}
               />
             ))}
+            <Text
+              style={{
+                textAlign: 'center',
+                padding: 10,
+                borderBottomWidth: 1,
+                borderBottomColor: theme.colors.border,
+              }}
+            >
+              Markpreis berechnet um{' '}
+              {format(
+                ReallifeService.getChangelogDate(market.data[0].market[0].updated_at),
+                'HH:mm:ss'
+              )}{' '}
+              Uhr
+            </Text>
           </List.Section>
         ) : (
           <NoContent />
