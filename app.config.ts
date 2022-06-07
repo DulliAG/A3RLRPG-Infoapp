5beeb2d939f1caf8e3b5680f1889cc1a63d9b0be
Thorben <38816229+tklein1801@users.noreply.github.com>
2022-06-07 10:54:39 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ export default {
     android: {
       permissions: ['NOTIFICATIONS'],
       googleServicesFile: './google-services.json',
-      versionCode: 1,
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: './assets/dag-blobbo.png',
         backgroundColor: '#fff',
